7759fc596fb8a2d2a4bb4e78d10fbeeca168bb39
Raymond <extraymond@gmail.com>
2019-07-06 08:33:20 +0800

    update dist

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -72743,7 +72743,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38639" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42523" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
